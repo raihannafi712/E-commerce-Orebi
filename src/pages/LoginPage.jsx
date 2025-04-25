5985b7fe3e8255d56a31db888923d909da3ddc3a
raihannafi712 <shitabraihan712@gmail.com>
2025-04-26 00:20:03 +0600

    demo

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,9 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import Container from './../components/Container';
 import { Link } from "react-router-dom";
 import { FaAngleRight } from "react-icons/fa";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { TiTick } from "react-icons/ti";
+
 
 const LoginPage = () => {
+
+  const auth = getAuth();
+  let [email , setEmail] = useState("");
+  let [password , setPassword] = useState("");
+  let [error , setError] = useState();
+  let [complete , setComplete] = useState();
+  
+
+  let handleEmail = (e) => {
+    setEmail(e.target.value);
+  }
+
+  let handlePassword = (e) =>{
+    setPassword(e.target.value);
+  }
+
+  let handleSubmit = () => {
+    if (email == "") {
+        setError("Please enter your Email!");     
+
+    } else if (password == "") {
+        setError("Please enter your password!");
+        
+    }else {
+      signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        setComplete(complete);
+        setError("Successfully logged in!");      
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        setError("Wrong password!");
+        
+      });
+    }
+  }
+
   return (
     <section>
       <Container>
@@ -17,26 +58,71 @@ const LoginPage = () => {
         </div>   
         <div className="py-[50px]">
           <div className="w-[30%] mx-auto ">
-            <form class="max-w-sm mx-auto">
-              <div class="mb-5">
-                <label for="email" className="block mb-2 text-[18px] font-dm font-bold text-black ">Your Email</label>
-                <input type="email" id="email" className="bg-white  text-black text-sm border-black border-[1px] rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@gmail.com" required />
-              </div>
-              <div class="mb-5">
-                <label for="password"  className="block mb-2 text-[18px] font-dm font-bold text-black ">Your password</label>
-                <input type="password" id="password" className="bg-white  text-black text-sm border-black border-[1px] rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
-              </div>
-              <div class="flex items-start mb-5">
-                <div class="flex items-center h-5">
-                  <input id="remember" type="checkbox" value="" class="w-4 h-4 border border-gray-300 rounded-sm bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800" required />
+              <form class="max-w-sm mx-auto">
+                <div class="mb-5">
+                  <label for="email" className="block mb-2 text-[18px] font-dm font-bold text-black ">Your Email</label>
+                  <input onChange={handleEmail} type="email" id="email" className="bg-white  text-black text-sm border-black border-[1px] rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@gmail.com"  />
                 </div>
-                <label for="remember" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Remember me</label>
-              </div>
-              <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                Log in
-              </button>
-            </form>
+                <div class="mb-5">
+                  <label for="password"  className="block mb-2 text-[18px] font-dm font-bold text-black ">Your password</label>
+                  <input type="password" onChange={handlePassword} id="password" className="bg-white  text-black text-sm border-black border-[1px] rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+                </div>
+                <div class="flex items-start mb-5">
+                  <div class="flex items-center h-5">
+                    <input id="remember" type="checkbox" value="" class="w-4 h-4 border border-gray-300 rounded-sm bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800" />
+                  </div>
+                  <label for="remember" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Remember me</label>
+                </div>
+                <div className="flex gap-x-[20px] items-center ">
+                  <a onClick={handleSubmit} className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 cursor-pointer">
+                    Log in
+                  </a>
+                  <h3 className="font-dm font-medium text-red-700 ">
+                      {error}
+                  </h3>
+                </div>
+              </form>
           </div>
+          {/* {!complete 
+            ?
+            <>
+              <div className="flex justify-center gap-x-[10px] items-center ">
+                <div className="text-[20px] text-white p-[10px] bg-black rounded-[100%] ">
+                  <TiTick />
+                </div>
+                <h3 className="font-dm font-bold text-[20px]  ">
+                  Successfully logged in!
+                </h3>
+              </div>
+            </>
+            :
+            <div className="w-[30%] mx-auto ">
+              <form class="max-w-sm mx-auto">
+                <div class="mb-5">
+                  <label for="email" className="block mb-2 text-[18px] font-dm font-bold text-black ">Your Email</label>
+                  <input onChange={handleEmail} type="email" id="email" className="bg-white  text-black text-sm border-black border-[1px] rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@gmail.com"  />
+                </div>
+                <div class="mb-5">
+                  <label for="password"  className="block mb-2 text-[18px] font-dm font-bold text-black ">Your password</label>
+                  <input type="password" onChange={handlePassword} id="password" className="bg-white  text-black text-sm border-black border-[1px] rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+                </div>
+                <div class="flex items-start mb-5">
+                  <div class="flex items-center h-5">
+                    <input id="remember" type="checkbox" value="" class="w-4 h-4 border border-gray-300 rounded-sm bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800" />
+                  </div>
+                  <label for="remember" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Remember me</label>
+                </div>
+                <div className="flex gap-x-[20px] items-center ">
+                  <a onClick={handleSubmit} className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 cursor-pointer">
+                    Log in
+                  </a>
+                  <h3 className="font-dm font-medium text-red-500 ">
+                      {error}
+                  </h3>
+                </div>
+              </form>
+            </div>
+          } */}
         </div>
       </Container>
     </section>
--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -1,9 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import Container from './../components/Container';
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { FaAngleRight } from "react-icons/fa";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
 const SignUpPage = () => {
+
+    const auth = getAuth();
+    let [email , setEmail] = useState("");
+    let [password , setPassword] = useState("");
+    let navigate = useNavigate();                   /* To navigate to login page */
+    let [error , setError] = useState();
+
+
+    let handleEmail = (e) => {
+    setEmail(e.target.value);
+    }
+
+    let handlePassword = (e) =>{
+    setPassword(e.target.value);
+    }
+
+    let handleSubmit = () => {
+        if (email == "") {
+            setError("Please enter your Email!");     
+
+        } else if (password == "") {
+            setError("Please set a new password!");
+            
+        }else {
+            createUserWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+                navigate("/login")
+            // console.log("done");         /* to check */
+            })
+            .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            // console.log("oops");
+            });
+        }
+    }
   return (
     <section>
         <Container>
@@ -29,7 +66,7 @@ const SignUpPage = () => {
                         </div>
                         <div className="">
                             <h3 className="font-dm font-bold text-[18px] pt-[50px] pb-2  ">Email Address</h3>
-                            <input type="text" placeholder="company@domain.com"
+                            <input onChange={handleEmail} type="email" id="email" placeholder="company@domain.com"
                             className="border-b-2 "             
                             />
                         </div>
@@ -109,7 +146,7 @@ const SignUpPage = () => {
                     <div className="w-[20%] ">
                         <div className="">
                             <h3 className="font-dm font-bold text-[18px] pt-[50px] pb-2  ">Password</h3>
-                            <input type="text" placeholder="Password"
+                            <input type="password" onChange={handlePassword} id="password" placeholder="Password"
                             className="border-b-2 "             
                             />
                         </div>
@@ -140,10 +177,13 @@ const SignUpPage = () => {
                         </div>
                     </div>
                 </div>
-                <div className="py-[70px] ">
-                    <a href="#" className="font-dm lg:text-[14px] text-[12px] font-bold text-center text-[#262626]  py-[8px] lg:py-[15px] px-[47px]  cursor-pointer duration-300 ease-in-out hover:text-[white] hover:font-bold  hover:bg-[#2B2B2B] border-[1px] border-gray-300 mr-6 ">
+                <div className="py-[70px] flex gap-x-[20px] items-center ">
+                    <a onClick={handleSubmit} className="font-dm lg:text-[14px] text-[12px] font-bold text-center text-[#262626]  py-[8px] lg:py-[15px] px-[47px]  cursor-pointer duration-300 ease-in-out hover:text-[white] hover:font-bold  hover:bg-[#2B2B2B] border-[1px] border-gray-300 mr-6 ">
                      Sign up
                     </a>
+                    <h3 className="font-dm font-medium text-red-500 ">
+                        {error}
+                    </h3>
                 </div>
                 <div className="flex items-center pb-[20px] ">
                     <h3 className="font-dm lg:text-[14px] text-[12px] font-medium text-black">Already a member?</h3>
